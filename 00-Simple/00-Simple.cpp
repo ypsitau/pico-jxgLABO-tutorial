9296341d7a9bd4c9c4dd1f35ae3e64bee1339c82
ypsitau <ypsitau@nifty.com>
2025-09-08 16:33:08 +0900

    added samples

--- 00-Simple/00-Simple.cpp
+++ 00-Simple/00-Simple.cpp
@@ -2,13 +2,11 @@
 #include "pico/stdlib.h"
 #include "jxglib/LABOPlatform.h"
 
-using namespace jxglib;
-
 int main()
 {
-	::stdio_init_all();
-	LABOPlatform::Instance.Initialize();
+	stdio_init_all();
+	jxglib_labo_init(false);
 	while (true) {
-		Tickable::Tick();
+		jxglib_tick();
 	}
 }
--- 01-Blink/01-Blink.cpp
+++ 01-Blink/01-Blink.cpp
@@ -6,7 +6,12 @@ int main()
 {
 	stdio_init_all();
 	jxglib_labo_init(false);
+	gpio_init(15);
+	gpio_set_dir(15, GPIO_OUT);
+	bool value = false;
 	while (true) {
-		jxglib_tick();
+		gpio_put(15, value);
+		jxglib_sleep(100);
+		value = !value;
 	}
 }
