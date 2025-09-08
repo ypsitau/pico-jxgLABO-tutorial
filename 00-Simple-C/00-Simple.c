9ed69cd10d44c8240ac5ab249f52f1dd03bc82d7
ypsitau <ypsitau@nifty.com>
2025-09-08 16:02:31 +0900

    updated

---
+++ 00-Simple-C/00-Simple.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "jxglib/LABOPlatform.h"
+
+int main()
+{
+	stdio_init_all();
+	jxglib_labo_init(false);
+	while (true) {
+		jxglib_tick();
+	}
+}
